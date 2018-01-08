4aee763feabe2a455f7d59919a4ff8dd14a07164
andybond13 <andybond13@yahoo.com>
2018-01-07 17:33:51 -0800

    Add methods to calculate the moment tensors (N)

--- fabric_tensors_python/fabric_tensor.py
+++ fabric_tensors_python/fabric_tensor.py
@@ -7,21 +7,86 @@ import sys
 def read_file(filename):
     A = np.loadtxt(filename)
     #x,y,z, weight
-    return A 
+    return A
+
+def reduce_dimension(data):
+    #reduce 3d data (x,y,z) into 2d data (r,z)
+    #assumed independent dimension = z, reduced dimensions = x,y
+    shape3d = data.shape
+    shape2d = (shape3d[0], 3)    #r,z,w
+    data2d = np.zeros(shape2d)
+    assert(shape3d[1] == 3 or shape3d[1] == 4)
+
+    for i in range(0, shape2d[0]):
+        x = data[i,0]
+        y = data[i,1]
+        z = data[i,2]
+        if (shape3d[1] == 3):
+            w = 1.0
+        elif (shape3d[1] == 4):
+            w = data[i,3]
+        r = np.sqrt( np.multiply(x,x) + np.multiply(y,y) ) 
+        data2d[i,0] = r 
+        data2d[i,1] = z 
+        data2d[i,2] = w 
+    return data2d 
+
+def calculate_N(data, dimension):
+
+    n = data.shape[0]
+    assert( data.shape[1] == dimension + 1 )
+
+    #N0
+    N0 = 1
+
+    #N2
+    N2 = np.zeros( (dimension, dimension) )
+    for i in range(0, dimension):
+        for j in range(0, dimension):
+            N2[i,j] = np.dot(data[:,i], data[:,j]) / n
+
+    #N4
+    N4 = np.ndarray( (dimension, dimension, dimension, dimension) )
+    for i in range(0, dimension):
+        for j in range(0, dimension):
+            for k in range(0, dimension):
+                for l in range(0, dimension):
+                        N4[i,j,k,l] = np.dot( np.multiply(data[:,i], data[:,j]) , np.multiply(data[:,k], data[:,l]) ) / n
+
+    print "N0",N0
+    print "N2",N2
+    print "N4",N4
+
+    return N0, N2, N4
 
 def calc_FT(files, dimension, weighted):
     for filename in files:
         print filename
         data = read_file(filename)
-        print data
+
+        if (dimension == 2):
+            print "*Reducing data dimension"
+            data = reduce_dimension(data)
+
+        #calculate fabric tensors of first kind (moment tensors, N)
+        N0, N2, N4 = calculate_N(data, dimension)
+        #calculate fabric tensors of first kind (fabric tensors, F)
+#        F0, F2, F4 = calculate_F(data)
+        #calculate fabric tensors of first kind (deviator tensors, D)
+#        D0, D2, D4 = calculate_D(data)
+
+        #calculate statistical significance
+        #**********  
+ 
     return
 
+
 if __name__ == "__main__":
-    optlist,args = getopt.getopt(sys.argv[1:],'',longopts=['3D','weighted'])
+    optlist,args = getopt.getopt(sys.argv[1:],'',longopts=['3D','3d','weighted'])
     dimension = 2
     weighted = 0
     for item in optlist:
-        if (item[0] == '--3D'):
+        if (item[0].lower() == '--3d'):
             dimension = 3 
         elif (item[0] == '--weighted'):
             weighted = 1
--- fabric_tensors_python/run.sh
+++ fabric_tensors_python/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 time /sw/bin/python2.7 fabric_tensor.py 90wt_0bar_contact_contact.txt
+time /sw/bin/python2.7 fabric_tensor.py --3d 90wt_0bar_contact_contact.txt
+
+
+
+#need to add weighting
+#need to add 2d/3d option
+#need to add plotting
