8f8fdb80fbd9438173df05b969dfd8ea82016b53
andybond13 <andybond13@yahoo.com>
2018-01-12 09:46:23 -0800

    Add draft of plotting script. 3d and 3d_polar2d are done.

--- fabric_tensors_python/run.sh
+++ fabric_tensors_python/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-time /sw/bin/python2.7 fabric_tensor.py 90wt_0bar_contact_contact.txt
-time /sw/bin/python2.7 fabric_tensor.py --3d 90wt_0bar_contact_contact.txt
+#time /sw/bin/python2.7 plot_fabric_tensor.py 90wt_0bar_contact_contact.txt
+time /sw/bin/python2.7 plot_fabric_tensor.py --3d 90wt_0bar_contact_contact.txt
 
 
 #look into anisotropy measure ../fabric_tensors/tensor/fractional_anisotropy.m
+#https://en.wikipedia.org/wiki/Fractional_anisotropy
 #need to add weighting
-#need to add plotting
+#need to add plotting 2d
+#plot raw vectors  / bar plots (2d)
+
+#make kanatani test problem
