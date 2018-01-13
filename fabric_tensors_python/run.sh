cbe0c7fc24d68edf9705f631d6e4ff8b22aebf76
andybond13 <andybond13@yahoo.com>
2018-01-13 15:47:18 -0800

    Add test cases for 3d and 2d cases.

--- fabric_tensors_python/run.sh
+++ fabric_tensors_python/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-time /sw/bin/python2.7 plot_fabric_tensor.py contact_test_2d.txt 
+#time /sw/bin/python2.7 plot_fabric_tensor.py contact_test_2d.txt 
+time /sw/bin/python2.7 plot_fabric_tensor.py --3d contact_test_3d.txt 
 #time /sw/bin/python2.7 plot_fabric_tensor.py 90wt_0bar_contact_contact.txt
 #time /sw/bin/python2.7 plot_fabric_tensor.py --3d 90wt_0bar_contact_contact.txt
 
@@ -8,5 +9,3 @@ time /sw/bin/python2.7 plot_fabric_tensor.py contact_test_2d.txt
 #https://en.wikipedia.org/wiki/Fractional_anisotropy
 #need to add weighting
 #plot raw vectors  / bar plots (2d)
-
-#make kanatani test problem
