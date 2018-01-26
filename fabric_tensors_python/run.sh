5c84e9aaccd97aba58e118b9facd18b8f9ca0c26
andybond13 <andybond13@yahoo.com>
2018-01-25 21:23:05 -0800

    Update run.sh with to-do list.

--- fabric_tensors_python/run.sh
+++ fabric_tensors_python/run.sh
@@ -7,7 +7,6 @@ time /sw/bin/python2.7 plot_fabric_tensor.py --3d --plot --raw contact_test_3d.t
 #time /sw/bin/python2.7 plot_fabric_tensor.py --plot --3d --write=90wt_0bar.ft --png=90wt_0bar.png 90wt_0bar_contact_contact.txt
 
 
-#confirm stats. find an example; maybe compare against mmtensor (also... I ommitted the symmetrization of indices in calculation of F4+, D4+, and stat4+)
-#examine mmtensor code, compare code
 #make an example problem, compare to MMTensor
 #confirm/debug writing to files / saving files
+#I don't trust polar plot. for data=I(3), it's not the same magnitude in Z & r. Need some correction factor for r I think.
